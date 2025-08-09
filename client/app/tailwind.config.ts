f2ff46ebb39d8c132dc70ca2cd5fd6cf7be9bf8e
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-09 06:51:16 -0400

    refactor: use ES module import for tailwindcss-animate

--- client/app/tailwind.config.ts
+++ client/app/tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import animate from "tailwindcss-animate";
 
 const config: Config = {
   darkMode: ["class"],
@@ -81,6 +82,6 @@ const config: Config = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 };
 export default config;

4958b6731ac40c6a5b465b9088214bb9dab30afb
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-10 16:32:07 -0400

    chore: consolidate client sources under src

--- client/src/components/forms/ListingForm.tsx
+++ client/src/components/forms/ListingForm.tsx
@@ -3,7 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { Form } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
-import { CustomFormField } from "../FormField";
+import { CustomFormField } from "@/components/FormField";
 import { createListing } from "@/lib/api";
 
 const listingSchema = z.object({

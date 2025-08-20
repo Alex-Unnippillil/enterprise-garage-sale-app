238cca05d50a5061eca1bdb9ebf40dd74c5047b0
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 00:00:58 -0400

    Fix conflicts: removed problematic lines from 12 files

--- server/src/routes/flag-routes.ts
+++ server/src/routes/flag-routes.ts
@@ -1,9 +1,1 @@
-import express from 'express';
-import { getFlags, updateFlag } from '../controllers/flag-controllers';
-
-const router = express.Router();
-
-router.get('/', getFlags);
-router.put('/:name', updateFlag);
-
-export default router;
+﻿

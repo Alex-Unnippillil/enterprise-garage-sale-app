e331085c6034f9e92125bc5e4619f102da6b87b0
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-20 00:00:42 -0400

    Fix conflicts: removed problematic lines from 11 files

--- server/src/controllers/analytics-controllers.ts
+++ server/src/controllers/analytics-controllers.ts
@@ -1,16 +1,1 @@
-import { Request, Response, NextFunction } from 'express';
-import { getManagerAnalytics } from '../services/analytics-service';
-
-export const getAnalytics = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): Promise<void> => {
-  try {
-    const { cognitoId } = req.params;
-    const analytics = await getManagerAnalytics(cognitoId);
-    res.json(analytics);
-  } catch (err) {
-    next(err);
-  }
-};
+﻿
--- server/src/routes/manager-routes.ts
+++ server/src/routes/manager-routes.ts
@@ -1,18 +1,1 @@
-import express from "express";
-import {
-  getManager,
-  createManager,
-  updateManager,
-  getManagerProperties,
-} from "../controllers/manager-controllers";
-import { getAnalytics } from "../controllers/analytics-controllers";
-
-const router = express.Router();
-
-router.get("/:cognitoId", getManager);
-router.put("/:cognitoId", updateManager);
-router.get("/:cognitoId/properties", getManagerProperties);
-router.get("/:cognitoId/analytics", getAnalytics);
-router.post("/", createManager);
-
-export default router;
+﻿

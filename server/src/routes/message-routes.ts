ba52cd8281d78a84b5bd86cfe77ef6ee2945999d
alex.unnippillil <alex.unnippillil@hotmail.com>
2025-08-19 21:43:04 -0400

    Fix conflicts: removed problematic lines from 14 files

--- server/src/routes/message-routes.ts
+++ server/src/routes/message-routes.ts
@@ -1,9 +1,1 @@
-import express from 'express';
-import { getMessages } from '../controllers/message-controllers';
-import { authMiddleware } from '../middleware/auth-middleware';
-
-const router = express.Router();
-
-router.get('/:conversationId', authMiddleware(['tenant', 'manager']), getMessages);
-
-export default router;
+﻿

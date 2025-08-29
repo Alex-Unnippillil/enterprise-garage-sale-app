82d094b760313ac3220e89b7ed7772009ed875bc
Alex Unnippillil <alex.unnippillil@hotmail.com>
2025-08-28 20:52:42 -0700

    feat: add slide viewer with document picture-in-picture

---
+++ client/src/app/(nondashboard)/demo/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SlideViewer from "@/components/slide-viewer";
+
+const DemoPage = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl mb-4">Slide Demo</h1>
+      <SlideViewer />
+    </div>
+  );
+};
+
+export default DemoPage;
+
